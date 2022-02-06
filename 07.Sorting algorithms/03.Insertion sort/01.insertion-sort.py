b9673f048e01e6181d20e464999aaf3a2bc6633b
shakil-babu <personal.shakil.babu@gmail.com>
2022-02-06 14:46:59 +0600

    insertion-sort

--- 07.Sorting algorithms/03.Insertion sort/01.insertion-sort.py
+++ 07.Sorting algorithms/03.Insertion sort/01.insertion-sort.py
@@ -1,10 +1,38 @@
-def insertionSort(arr):
-    for i in range(1, len(arr)):
-        item = arr[i]
+# def insertionSort(arr):
+#     for i in range(1, len(arr)):
+#         item = arr[i]
 
+#         j = i - 1
+#         while j >= 0 and arr[j] > item:
+#             arr[j+1] = arr[j]
+#             j = j - 1
+#         arr[j+1] = item
+#     return arr
+
+
+def insertionSort(arr):
+    # i এর মান 1 থেকে len(arr) এর আগ পর্যন্ত 1 করে বাড়াই
+    for i in range(1, len(arr)):
+        # arr[i] কে currentValue রে এসাইন করি
+        currentValue = arr[i]
+
+        # currentValue এর জন্য উপযুক্ত স্থান খুঁজে বের করি
         j = i - 1
-        while j >= 0 and arr[j] > item:
+
+        # যদি j, 0 থেকে সমান বা বড় হয় এবং  arr[j] থেকে currentValue ছোট হয়
+        while j >= 0 and arr[j] > currentValue:
+
+            # arr[j] কে তার পরের ঘরে রেখে দিই
             arr[j+1] = arr[j]
-            j = j - 1
-            arr[j+1] = item
+
+            # j এর মান 1 করে কমাই
+            j -= 1
+
+        # এখন খালি জায়গায় currentValue কে বসাই
+        arr[j+1] = currentValue
+
     return arr
+
+
+ans = insertionSort([500, 200, 100, 400])
+print(ans)
