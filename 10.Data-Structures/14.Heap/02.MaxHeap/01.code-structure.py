decf880b1db96edc6602a6a43a33fb325d191c0c
shakil-babu <personal.shakil.babu@gmail.com>
2022-02-25 02:08:01 +0600

    max-heap-insert

--- 10.Data-Structures/14.Heap/02.MaxHeap/01.code-structure.py
+++
@@ -1,3 +0,0 @@
-class MaxHeap:
-    def __init__(self):
-        self.values = []
--- 10.Data-Structures/code.py
+++ 10.Data-Structures/code.py
@@ -23,17 +23,53 @@ class MaxHeap:
                 idx = parentIndex
 
     # _maxHeapify method
-    def _maxHeapify(self):
-        pass
+    def _maxHeapify(self, index=0):
+        left = index * 2 + 1
+        right = index * 2 + 2
+
+        length = len(self.values)
+        largest = index
+
+        if left < length and self.values[largest] < self.values[left]:
+            largest = left
+
+        if right < length and self.values[largest] < self.values[right]:
+            largest = right
+
+        if largest != index:
+            temp = self.values[index]
+            self.values[index] = self.values[largest]
+            self.values[largest] = temp
+
+            # recursively called
+            self._maxHeapify()
+
+    # exactMax method
+    def _exactMax(self):
+        maxElement = self.values[0]
+        self.values[0] = self.values.pop()
+        self._maxHeapify()
+        return maxElement
 
     # size method
     def _size(self):
-        pass
+        return len(self.values)
 
+    # isEmpty method
+    def _isEmpty(self):
+        return len(self.values) == 0
+
+    # getMax method
+    def _getMax(self):
+        if len(self.values) == 0:
+            return "Heap is empty!"
+        else:
+            return self.values[0]
 
     # print heap
-    def _printHeap(self) :
+    def _printHeap(self):
         print(self.values)
+
 
 # initialization
 maxheap = MaxHeap()
@@ -44,5 +80,6 @@ maxheap._insert(200)
 maxheap._insert(1000)
 
 
-# print 
+print(maxheap._exactMax())
+# print
 maxheap._printHeap()
