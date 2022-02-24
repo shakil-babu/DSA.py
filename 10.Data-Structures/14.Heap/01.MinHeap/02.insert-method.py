7f6be229a3764d9919eb5f1553d9173d24fe2f54
shakil-babu <personal.shakil.babu@gmail.com>
2022-02-24 18:44:01 +0600

    MinHeap-insert

---
+++ 10.Data-Structures/14.Heap/01.MinHeap/02.insert-method.py
@@ -0,0 +1,17 @@
+def _insert(self, val):
+    self.values.append(val)
+    self._bubbleUp()
+
+
+def _bubbleUp(self):
+    idx = len(self.values) - 1
+    element = self.values[idx]
+    while idx > 0:
+        parentIdx = idx + 1 // 2
+        parentElement = self.values[parentIdx]
+        if parentElement < element:
+            break
+        else:
+            self.values[idx] = parentElement
+            self.values[parentIdx] = element
+            idx = parentIdx
