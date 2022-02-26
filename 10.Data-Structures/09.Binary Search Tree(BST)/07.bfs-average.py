b50aa339f5153a1f70727983f960f4aa52da6951
shakil-babu <personal.shakil.babu@gmail.com>
2022-02-26 12:55:41 +0600

    bfs-average-bst

--- 10.Data-Structures/09.Binary Search Tree(BST)/07.bfs-average.py
+++ 10.Data-Structures/09.Binary Search Tree(BST)/07.bfs-average.py
@@ -1,26 +1,26 @@
 
-  def BFS_average(self):
-       if self.root is None:
-            return []
+def BFS_average(self):
+    if self.root is None:
+        return []
 
-        data = []
-        queue = [self.root]
-        while len(queue):
-            hegith = []
-            length = len(queue)
-            for i in range(length):
-                current = queue.pop(0)
-                hegith.append(current.val)
+    data = []
+    queue = [self.root]
+    while len(queue):
+        hegith = []
+        length = len(queue)
+        for i in range(length):
+            current = queue.pop(0)
+            hegith.append(current.val)
 
-                if current.left:
-                    queue.append(current.left)
-                if current.right:
-                    queue.append(current.right)
-            data.append(hegith)
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+        data.append(hegith)
 
-        ans = []
-        for item in data:
-            avg = sum(item) / len(item)
-            ans.append(avg)
+    ans = []
+    for item in data:
+        avg = sum(item) / len(item)
+        ans.append(avg)
 
-        return ans
+    return ans
