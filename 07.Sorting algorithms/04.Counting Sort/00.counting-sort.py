488292bd61a193c8f711cf0d75032625eb2c5a38
shakil-babu <personal.shakil.babu@gmail.com>
2022-03-03 16:25:00 +0600

    counting-sort-algorithm

--- 07.Sorting algorithms/04.Counting Sort/00.counting-sort.py
+++ 07.Sorting algorithms/04.Counting Sort/00.counting-sort.py
@@ -1,49 +1,17 @@
 from collections import Counter
 
 
-# def countingSort(arr):
-#     c = Counter(arr)
-#     minv = min(c.keys())
-#     maxv = max(c.keys())
+def countingSort(arr):
+    c = Counter(arr)
+    minv = min(c.keys())
+    maxv = max(c.keys())
 
-#     sortedList = []
-#     for i in range(minv, maxv):
-#         sortedList.extend([i] * c[i])
-#     return sortedList
-
-
-# # initialization
-# sorting = countingSort([3, 4, 6, 2, -1, -1, -2, -3, 4, 4, ])
-# print(sorting)
-
-def merge(a, b):
-    i, j = 0, 0
-    ans = []
-    while i < len(a) and j < len(b):
-        if a[i] < b[j]:
-            ans.append(a[i])
-            i += 1
-        else:
-            ans.append(b[j])
-            j += 1
-
-    if i < len(a):
-        ans.extend(a[i:])
-    elif j < len(b):
-        ans.extend(b[j:])
-
-    return ans
-
-
-def mergeSort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-    left, right = mergeSort(arr[:mid]), mergeSort(arr[mid:])
-    return merge(left, right)
+    sortedList = []
+    for i in range(minv, maxv):
+        sortedList.extend([i] * c[i])
+    return sortedList
 
 
 # initialization
-sorting = mergeSort([5, 2, 3, 1])
+sorting = countingSort([3, 4, 6, 2, -1, -1, -2, -3, 4, 4, ])
 print(sorting)
