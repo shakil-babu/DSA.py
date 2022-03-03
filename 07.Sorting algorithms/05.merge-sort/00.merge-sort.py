8bc516f1dd99634e19dcbb6f4f313e944645fa21
shakil-babu <personal.shakil.babu@gmail.com>
2022-03-03 16:24:13 +0600

    merge-sort-algorithm

--- 07.Sorting algorithms/05.merge-sort/00.merge-sort.py
+++ 07.Sorting algorithms/05.merge-sort/00.merge-sort.py
@@ -1,21 +1,3 @@
-from collections import Counter
-
-
-# def countingSort(arr):
-#     c = Counter(arr)
-#     minv = min(c.keys())
-#     maxv = max(c.keys())
-
-#     sortedList = []
-#     for i in range(minv, maxv):
-#         sortedList.extend([i] * c[i])
-#     return sortedList
-
-
-# # initialization
-# sorting = countingSort([3, 4, 6, 2, -1, -1, -2, -3, 4, 4, ])
-# print(sorting)
-
 def merge(a, b):
     i, j = 0, 0
     ans = []
