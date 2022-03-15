1fcca65bedb4209af33dd55344c1664aa851ed99
shakil-babu <personal.shakil.babu@gmail.com>
2022-03-15 09:11:10 +0600

    get-item

--- 10.Data-Structures/15.Hash Table/04.__getItem__method.py
+++ 10.Data-Structures/15.Hash Table/04.__getItem__method.py
@@ -1,3 +1,11 @@
+# def __getItem__(self, key) :
+#     hash = self.__hashing__(key)
+#     return self.arr[hash]
+
+
 def __getItem__(self, key) :
-    hash = self.__hashing__(key)
-    return self.arr[hash]
+        hash = self.__hashing__(key)
+
+        for item in self.arr[hash]:
+            if item[0] == key :
+                return item[1]
