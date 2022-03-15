c4f09b47a1be2170cd0ca2b26350b18882c1544f
shakil-babu <personal.shakil.babu@gmail.com>
2022-03-15 09:11:35 +0600

    delete-item

--- 10.Data-Structures/15.Hash Table/05.__deleteItem__method.py
+++ 10.Data-Structures/15.Hash Table/05.__deleteItem__method.py
@@ -1,3 +1,10 @@
+# def __deleteItem__(self, key) :
+#     hash = self.__getItem__(key)
+#     self.arr[hash] = None
+
+
 def __deleteItem__(self, key) :
-    hash = self.__getItem__(key)
-    self.arr[hash] = None
+        hash = self.__hashing__(key)
+        for idx,element in enumerate(self.arr[hash]):
+            if element[0] == key :
+                del self.arr[hash][idx] 
