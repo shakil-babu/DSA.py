c115ce91be80bd11d60e73c6056a04e23e325857
shakil-babu <personal.shakil.babu@gmail.com>
2022-03-15 09:12:18 +0600

    hashtable

--- 10.Data-Structures/15.Hash Table/hashtable.py
+++ 10.Data-Structures/15.Hash Table/hashtable.py
@@ -1,9 +1,7 @@
 class HashTable :
     def __init__(self) :
         self.Max = 100
-        self.arr = [None for i in range(self.Max)]
-
-
+        self.arr = [[] for i in range(self.Max)]
     
     # hashfunction
     def __hashing__(self, key) :
@@ -17,32 +15,52 @@ class HashTable :
     # setItem method
     def __setItem__(self,key, val) :
         hash = self.__hashing__(key)
-        self.arr[hash] = val
+        found = False
+        for idx,element in enumerate(self.arr[hash]):
+            if len(element) == 2 and element[0] == key :
+                self.arr[hash].append((key,val))
+                found = True
+                break
 
-
+        if not found:
+            self.arr[hash].append((key,val))
+            
     # get item
     def __getItem__(self, key) :
         hash = self.__hashing__(key)
-        return self.arr[hash]
 
+        for item in self.arr[hash]:
+            if item[0] == key :
+                return item[1]
 
     # delete item
     def __deleteItem__(self, key) :
-        hash = self.__getItem__(key)
-        self.arr[hash] = None
+        hash = self.__hashing__(key)
+        for idx,element in enumerate(self.arr[hash]):
+            if element[0] == key :
+                del self.arr[hash][idx]             
 
-
+    # print
+    def Output(self) :
+        print(self.arr)
 
 # instance
 hashtable = HashTable()
 
 
 # insert data
-hashtable.__setItem__("shakil", 1001)
-hashtable.__setItem__("torikus", 10020)
+hashtable.__setItem__("abc", 1001)
+hashtable.__setItem__("bac", 1111)
+
+hashtable.__setItem__("baby", 10020)
+hashtable.__setItem__("bayb", 100)
 hashtable.__setItem__("fahim", 39002)
 
 
 # get item
-a = hashtable.__getItem__('shakil')
-print(a)
+a = hashtable.__getItem__('fahim')
+# print(a)
+# hashtable.Output()
+
+hashtable.__deleteItem__('baby');
+hashtable.Output()
