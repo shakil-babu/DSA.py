a7d3c26a06410a2d500eb03b00c71865b92e0c1d
shakil-babu <personal.shakil.babu@gmail.com>
2022-03-15 09:10:39 +0600

    collision-separate-chaining

--- 10.Data-Structures/15.Hash Table/03.__setItem__method.py
+++ 10.Data-Structures/15.Hash Table/03.__setItem__method.py
@@ -1,3 +1,16 @@
+# def __setItem__(self,key, val) :
+#     hash = self.__hashing__(key)
+#     self.arr[hash] = val
+
+
 def __setItem__(self,key, val) :
-    hash = self.__hashing__(key)
-    self.arr[hash] = val
+        hash = self.__hashing__(key)
+        found = False
+        for idx,element in enumerate(self.arr[hash]):
+            if len(element) == 2 and element[0] == key :
+                self.arr[hash].append((key,val))
+                found = True
+                break
+
+        if not found:
+            self.arr[hash].append((key,val))
